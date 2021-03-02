b7e90843b493b087cabb394b97a5fe33b505bc66
Amanda Kwong <amandakwong898@gmail.com>
2021-03-01 21:27:11 -0500

    added outputs

--- terraform/aws/dev/alb_cicd/outputs.tf
+++ terraform/aws/dev/alb_cicd/outputs.tf
@@ -1,3 +1,19 @@
 output "alb_cicd_dns_name" {
   value = "${module.alb_cicd.alb_cicd_dns_name}"
 }
+
+output "alb_cicd_arn" {
+  value = "${module.alb_cicd.alb_cicd_arn}"
+}
+
+output "jenkins_tg_arn" {
+  value = "${module.alb_cicd.jenkins_tg_arn}"
+}
+
+output "nexus_tg_arn" {
+  value = "${module.alb_cicd.nexus_tg_arn}"
+}
+
+output "sonarqube_tg_arn" {
+  value = "${module.alb_cicd.sonarqube_tg_arn}"
+}
